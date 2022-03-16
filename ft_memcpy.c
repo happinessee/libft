a4b62853b48700a061d48827bfbaea94119f1f3f
happinessee <gygur1206@gmail.com>
2022-03-16 13:12:42 +0900

    modify memmove, memcpy
    part1 mendatory clear

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hyojeong <hyojeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 20:36:58 by hyojeong          #+#    #+#             */
-/*   Updated: 2022/03/15 16:02:46 by hyojeong         ###   ########.fr       */
+/*   Updated: 2022/03/16 13:11:45 by hyojeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	str1 = (unsigned char *)src;
 	str2 = (unsigned char *)dst;
 	cnt = 0;
+	if (dst == src)
+		return (dst);
 	while (cnt < n)
 	{
 		str2[cnt] = str1[cnt];
